a035e3d322a430a220ce0da81740cdf25cbb728b
BaeSeokJae <bsjp9400@gmail.com>
2021-05-07 19:10:25 +0900

    Proceeding Problem4

--- Problem_1.js
+++ Problem_1.js
@@ -89,7 +89,31 @@ function problem3(arg) {
   return arr;
 }
 
+/**
+ * 세자리 수를 곱해 만들 수 있는 가장 큰 대칭수
+ * 앞에서부터 읽을 때나 뒤에서부터 읽을 때나 모양이 같은 수를 대칭수(palindrome)라고 부릅니다.
+ * 두 자리 수를 곱해 만들 수 있는 대칭수 중 가장 큰 수는 9009 (= 91 × 99) 입니다.
+ * 세 자리 수를 곱해 만들 수 있는 가장 큰 대칭수는 얼마입니까?
+ *
+ * 풀이
+ *
+ */
+function problem4() {
+  let maxSum = 0;
+  for (let i = 1000; i > 100; i--) {
+    for (let j = 1000; j > 100; j--) {
+      maxSum = i * j;
+      let reverseMaxSum = maxSum.toString().split("").reverse("").join("");
+      if (maxSum.toString() === reverseMaxSum) {
+        return (maxSum = parseInt(maxSum));
+      }
+    }
+  }
+  return maxSum;
+}
+
 // const result = problem2(4000000);
-const result = problem3(600851475143);
+// const result = problem3(600851475143);
+const result = problem4();
 
 console.log(result);
